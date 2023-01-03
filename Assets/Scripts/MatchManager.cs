838f5f2fece4410ffdcced05e3e5f810ab0e4506
jerr-it <jrgls.no-reply@gmx.de>
2023-01-03 17:57:30 +0100

    Added new arena map, added map types

--- Assets/Scripts/MatchManager.cs
+++ Assets/Scripts/MatchManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.InputSystem;
 public class MatchManager : MonoBehaviour {
     [SerializeField] private List<Transform> spawnPoints;
     [SerializeField] private List<Sprite> playerSkins;
-    
     public void OnPlayerJoined(PlayerInput playerInput) {
         playerInput.transform.position = spawnPoints[playerInput.playerIndex].position;
         playerInput.gameObject.GetComponent<PlayerController>().SetPlayerSkin(playerSkins[playerInput.playerIndex]);
